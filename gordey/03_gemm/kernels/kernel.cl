8e3450662208630feb572e51293b8e48318dbfa1
dtaleme <kiwi0tails@gmail.com>
2022-12-12 13:47:50 +0300

    fix

--- gordey/03_gemm/kernels/kernel.cl
+++ gordey/03_gemm/kernels/kernel.cl
@@ -1,6 +1,6 @@
 __kernel void matmul (const unsigned int m, const unsigned int n, const unsigned int k, __global float* a, __global float* b, __global float* c) {
-    const unsigned int row = get_global_id(1);
-    const unsigned int col = get_global_id(0);
+    unsigned int row = get_global_id(1);
+    unsigned int col = get_global_id(0);
     float res = .0;
     for (unsigned int l = 0; l < n; l++) {
         res += a[row * n + l] * b[l * k + col];
@@ -9,14 +9,14 @@ __kernel void matmul (const unsigned int m, const unsigned int n, const unsigned
 }
 
 __kernel void gemm(const unsigned int m, const unsigned int n, const unsigned int k, __global float* a, __global float* b, __global float* c) {
-	const unsigned int row = get_local_id(1);
-	const unsigned int col = get_local_id(0);
+	unsigned int row = get_local_id(1);
+	unsigned int col = get_local_id(0);
 	
-	const unsigned int global_row = get_global_id(1);
-	const unsigned int global_col = get_global_id(0);
+	unsigned int global_row = get_global_id(1);
+	unsigned int global_col = get_global_id(0);
 	
-	__local float local_a[BLOCK][BLOCK];
-	__local float local_b[BLOCK][BLOCK];
+	__local float loc_a[BLOCK][BLOCK];
+	__local float loc_b[BLOCK][BLOCK];
 	
 	const unsigned int num_tiles = n / BLOCK;
 	
@@ -24,11 +24,11 @@ __kernel void gemm(const unsigned int m, const unsigned int n, const unsigned in
 	for (unsigned int t = 0; t < num_tiles; t++) {
 		const unsigned int tiled_i = BLOCK * t + row;
 		const unsigned int tiled_j = BLOCK * t + col;
-		local_a[row][col] = a[global_row * n + tiled_j];
-		local_b[row][col] = b[tiled_i * k + global_col];
+		loc_a[row][col] = a[global_row * n + tiled_j];
+		loc_b[row][col] = b[tiled_i * k + global_col];
 		barrier(CLK_LOCAL_MEM_FENCE);
 		for (unsigned int l = 0; l < BLOCK; l++) {
-			res += local_a[row][l] * local_b[l][col];
+			res += loc_a[row][l] * loc_b[l][col];
 		}
 		barrier(CLK_LOCAL_MEM_FENCE);
 	}
--- gordey/03_gemm/main.cpp
+++ gordey/03_gemm/main.cpp
@@ -5,71 +5,80 @@
 
 #include "include/matrix.h"
 
-#define m 512
-#define n 512
-#define k 512
+#define CLOCK std::chrono::high_resolution_clock::now()
 
-int main(int argc, char** argv) {
+int main(void) {
   std::vector<cl_platform_id> platforms;
 
   cl_uint count = getPlatforms(platforms);
   std::vector<std::pair<cl_platform_id, cl_device_id>> gpus, cpus;
 
   if (count < 1) {
-    throw -1;
+    std::cout << "Platform error\n";
+    return 0;
   }
+
+  std::cout << "Finding devices...\n";
 
   for (size_t i = 0; i < count; i++) {
     cl_platform_id platform = platforms[i];
 
-    cl_device_id gpu = getDevice(CL_DEVICE_TYPE_GPU, platform);
+    cl_device_id gpu = getDevice(1 << 2, platform);
     if (gpu != nullptr) gpus.push_back(std::make_pair(platform, gpu));
 
-    cl_device_id cpu = getDevice(CL_DEVICE_TYPE_CPU, platform);
+    cl_device_id cpu = getDevice(1 << 1, platform);
     if (cpu != nullptr) cpus.push_back(std::make_pair(platform, cpu));
   }
+
+  std::cout << "OK\n\n";
+
+  const int m = 1024;
+  const int n = 1024;
+  const int k = 1024;
 
   const size_t a_size = m * n;
   const size_t b_size = n * k;
   const size_t c_size = m * k;
-  float* a = new float[a_size];
-  float* b = new float[b_size];
-  float* c = new float[c_size];
-  float* c_ref = new float[c_size];
+  float* A = new float[a_size];
+  float* B = new float[b_size];
+  float* C = new float[c_size];
+  float* C_reference = new float[c_size];
 
-  createData<float>(a, a_size);
-  createData<float>(b, b_size);
+  createData<float>(A, a_size);
+  createData<float>(B, b_size);
 
-  auto t0 = std::chrono::high_resolution_clock::now();
-  matrix_multiplication(m, n, k, a, b, c);
-  auto t1 = std::chrono::high_resolution_clock::now();
+  std::cout << " --- MATRIX MULT ---\n\n";
+
+  auto t0 = CLOCK;
+  matrix_multiplication(m, n, k, A, B, C);
+  auto t1 = CLOCK;
   std::cout << "SEQ - " << TIME_MS(t0, t1) << std::endl;
-  std::memcpy(c_ref, c, c_size * sizeof(float));
+  std::memcpy(C_reference, C, c_size * sizeof(float));
 
-  t0 = std::chrono::high_resolution_clock::now();
-  matrix_multiplication_omp(m, n, k, a, b, c);
-  t1 = std::chrono::high_resolution_clock::now();
+  t0 = CLOCK;
+  matrix_multiplication_omp(m, n, k, A, B, C);
+  t1 = CLOCK;
   std::cout << "OMP - " << TIME_MS(t0, t1) << std::endl;
-  CHECK_CORRECT(true, float, c_ref, c, c_size)
+  CHECK_CORRECT(true, float, C_reference, C, c_size)
 
   timer time;
-  matrix_multiplication_gpu(m, n, k, a, b, c, gpus[0], time);
+  matrix_multiplication_gpu(m, n, k, A, B, C, gpus[0], time);
   std::cout << "GPU - " << TIME_MS(time.first, time.second) << std::endl;
-  CHECK_CORRECT(true, float, c_ref, c, c_size)
+  CHECK_CORRECT(true, float, C_reference, C, c_size)
 
-  matrix_multiplication_gpu(m, n, k, a, b, c, cpus[0], time);
+  matrix_multiplication_gpu(m, n, k, A, B, C, cpus[0], time);
   std::cout << "CPU - " << TIME_MS(time.first, time.second) << std::endl;
-  CHECK_CORRECT(true, float, c_ref, c, c_size)
+  CHECK_CORRECT(true, float, C_reference, C, c_size)
 
-  gemm(m, n, k, a, b, c, gpus[0], time);
+  gemm(m, n, k, A, B, C, gpus[0], time);
   std::cout << "GPU OPTIMIZED - " << TIME_MS(time.first, time.second)
             << std::endl;
-  CHECK_CORRECT(true, float, c_ref, c, c_size)
+  CHECK_CORRECT(true, float, C_reference, C, c_size)
 
-  gemm(m, n, k, a, b, c, cpus[0], time);
+  gemm(m, n, k, A, B, C, cpus[0], time);
   std::cout << "CPU OPTIMIZED - " << TIME_MS(time.first, time.second)
             << std::endl;
-  CHECK_CORRECT(true, float, c_ref, c, c_size)
+  CHECK_CORRECT(true, float, C_reference, C, c_size)
 
   return 0;
 }
